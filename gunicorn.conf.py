42e85b38ed6593c9f5da911964824141d2caf709
MrBlank <108883255+MrBlankCoding@users.noreply.github.com>
2024-08-18 22:31:42 -0400

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,4 +1,13 @@
 import multiprocessing
 
+# Bind to 0.0.0.0 to allow external access
 bind = "0.0.0.0:$PORT"
-workers = multiprocessing.cpu_count() * 2 + 1
+
+workers = 1
+
+
+max_requests = 1000
+max_requests_jitter = 50
+
+worker_connections = 1000
+preload_app = True
